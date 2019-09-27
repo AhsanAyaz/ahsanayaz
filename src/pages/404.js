c1c5d5b440864680f9d25618e94899252dd6f9e8
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2019-09-28 03:16:42 +0500

    feat(portals): Add MPA transition for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,6 +5,82 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 class NotFoundPage extends React.Component {
+  componentDidMount() {
+    if (typeof window !== 'undefined' && 'HTMLPortalElement' in window) {
+      this.addPortalToHome();
+    }
+  }
+
+  addPortalToHome() {
+    // Adding some styles with transitions
+    const style = document.createElement('style');
+    const initialScale = 0.4;
+    style.innerHTML = `
+      portal {
+        cursor: pointer;
+        position:fixed;
+        height: 500px;
+        width: 500px;
+        opacity: 0;
+        box-shadow: 0 0 20px 10px #999;
+        border-radius: 10%;
+        transform: scale(${initialScale});
+        bottom: calc(20px + 30% * 0.4 - 50%);
+        left: calc(100% * 0.4 - 50%);
+        top: 0;
+        right: 0;
+        margin: auto;
+        z-index: 10000;
+      }
+      .portal-transition {
+        transition:
+          transform 0.7s,
+          height 0.3s,
+          width 0.3s,
+          border-radius 0.1s,
+          opacity 1.0s;
+      }
+      @media (prefers-reduced-motion: reduce) {
+        .portal-transition {
+          transition: all 0.001s;
+        }
+      }
+      .portal-reveal {
+        transform: scale(1.0);
+        bottom: 0px;
+        left: 0px;
+        border-radius: 0;
+        width: 100%;
+        height: 100%;
+      }
+      .fade-in {
+        opacity: 1.0;
+      }
+    `;
+    const portal = document.createElement('portal');
+    // Let's navigate into the WICG Portals spec page
+    portal.src = 'https://ahsanayaz.com';
+    // Add a class that defines the transition. Consider using 
+    // `prefers-reduced-motion` media query to control the animation. 
+    // https://developers.google.com/web/updates/2019/03/prefers-reduced-motion
+    portal.classList.add('portal-transition');
+    portal.addEventListener('click', evt => {
+      // Animate the portal once user interacts
+      portal.classList.add('portal-reveal');
+    });
+    portal.addEventListener('transitionend', evt => {
+      if (evt.propertyName == 'transform') {
+        // Activate the portal once the transition has completed
+        portal.activate();
+      }
+    });
+    document.body.append(style, portal);
+
+    // Waiting for the page to load.
+    // using setTimeout is a suboptimal way and it's best to fade-in
+    // when receiving a load complete message from the portal via postMessage
+    setTimeout(_ => portal.classList.add('fade-in'), 800);
+  }
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
