52eaf2523932fc19084efb6d27c9514da417b925
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-02-16 02:02:00 +0100

    chore: add reload for when the app updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,12 @@ import "./src/styles/global.css"
 import "typeface-montserrat"
 import "typeface-merriweather"
 import "gatsby-remark-vscode/styles.css"
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `AhsanAyaz.com has been updated. ` + `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
