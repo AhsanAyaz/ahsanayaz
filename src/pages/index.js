155c1a357a9d5f114463ffdf326a97f8bde12d5f
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-03-07 17:14:09 +0100

    article: Angular Unit Tests with Jest - This constructor is not compatible with Angular Dependency Injection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HomeTags from "../components/HomeTags/HomeTags"
