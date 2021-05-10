4c0cc05867a1c875fb8184c9de2acc05083cda72
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-05-10 23:18:54 +0200

    feat(embedded-yt-videos): create a component for yt iframes

--- src/constants/short-codes.js
+++ src/constants/short-codes.js
@@ -2,9 +2,11 @@ import IonicCourse from "../components/IonicCourse/IonicCourse"
 import PromotionBanner from "../components/PromotionBanner/PromotionBanner"
 import ImageWithBg from "../components/ImageWithBg/ImageWithBg"
 import Support from "../components/Support/Support"
+import EmbeddedYouTubeVideo from "../components/EmbeddedYouTubeVideo/EmbeddedYouTubeVideo"
 
 const mdxShortCodes = {
   IonicCourse,
+  EmbeddedYouTubeVideo,
   PromotionBanner,
   ImageWithBg,
   Support,
