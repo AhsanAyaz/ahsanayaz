495456bf8a087551dc62ae9037302463579c997d
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-10-30 21:49:57 +0200

    chore(redirects): add redirects
    added nextjs redirects

--- next.config.js
+++ next.config.js
@@ -11,6 +11,72 @@ module.exports = withBundleAnalyzer({
   images: {
     domains: ['media.giphy.com', 'img.buymeacoffee.com', 'media1.tenor.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/ng-book',
+        destination:
+          'https://www.amazon.com/Angular-Cookbook-recipes-enterprise-scale-development-dp-1838989439/dp/1838989439/',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/youtube',
+        destination: 'https://youtube.com/c/CodewithAhsan',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/gde',
+        destination:
+          'https://developers.google.com/community/experts/directory/profile/profile-muhammad-ahsan-ayaz',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/muhd.ahsanayaz',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/facebook',
+        destination: 'https://facebook.com/muhd.ahsanayaz',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/tiktok',
+        destination: 'https://tiktok.com/@muhd.ahsanayaz',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/resume',
+        destination: 'https://linkedin.com/in/ahsanayaz',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://linkedin.com/in/ahsanayaz',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/muhd_ahsanayaz',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/author',
+        destination: 'https://www.amazon.com/~/e/B098TSDC9B',
+        permanent: true,
+        basePath: false,
+      },
+    ]
+  },
   target: 'serverless',
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
